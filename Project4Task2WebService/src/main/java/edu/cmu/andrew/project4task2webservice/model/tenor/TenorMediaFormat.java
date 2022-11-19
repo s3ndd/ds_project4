1d36b8e456565f90a7cf6af8e96d2603a3347d44
ZoeyChou <a14725836982@gmail.com>
2022-11-19 00:47:26 -0500

    Added inline comment to all file under "Model" folder

--- Project4Task2WebService/src/main/java/edu/cmu/andrew/project4task2webservice/model/tenor/TenorMediaFormat.java
+++ Project4Task2WebService/src/main/java/edu/cmu/andrew/project4task2webservice/model/tenor/TenorMediaFormat.java
@@ -1,3 +1,13 @@
+/**
+ * Co-Author: Sheldon Shi, I-Wen Chou
+ * AndrewID: lijuns, ichou
+ * Email: lijuns@andrew.cmu.edu, ichou@andrew.cmu.edu
+ * ProjectTask: Project4Task2
+ *
+ * This is a class for Tenor media format.
+ *
+ */
+
 package edu.cmu.andrew.project4task2webservice.model.tenor;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -5,21 +15,25 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TenorMediaFormat {
+    //tenor tiny gif
     @JsonProperty("tinygif")
     TenorTinyGif tenorTinyGif;
 
-
+    //constructor of TenorMediaFormat
     public TenorMediaFormat() {
     }
 
+    //get tenor tiny gif
     public TenorTinyGif getTenorTinyGif() {
         return tenorTinyGif;
     }
 
+    //set tenor tiny gif
     public void setTenorTinyGif(TenorTinyGif tenorTinyGif) {
         this.tenorTinyGif = tenorTinyGif;
     }
 
+    //override toString for print out
     @Override
     public String toString() {
         return "MediaFormat{" +
--- Project4Task2WebService/src/main/java/edu/cmu/andrew/project4task2webservice/model/tenor/TenorTinyGif.java
+++ Project4Task2WebService/src/main/java/edu/cmu/andrew/project4task2webservice/model/tenor/TenorTinyGif.java
@@ -1,3 +1,13 @@
+/**
+ * Co-Author: Sheldon Shi, I-Wen Chou
+ * AndrewID: lijuns, ichou
+ * Email: lijuns@andrew.cmu.edu, ichou@andrew.cmu.edu
+ * ProjectTask: Project4Task2
+ *
+ * This is a class for tenor tiny gif with url and dimension.
+ *
+ */
+
 package edu.cmu.andrew.project4task2webservice.model.tenor;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -6,28 +16,36 @@ import java.util.Arrays;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TenorTinyGif {
+    //tenor tiny gif url
     String url;
+    //tenor tiny gif dimension
     Integer[] dims;
 
+    //constructor of TenorTinyGif
     public TenorTinyGif() {
     }
 
+    //get tenor tiny gif url
     public String getUrl() {
         return this.url;
     }
 
+    //set tenor tiny gif url
     public void setUrl(String url) {
         this.url = url;
     }
 
+    //get tenor tiny gif dimension
     public Integer[] getDims() {
         return this.dims;
     }
 
+    //set tenor tiny gif dimension
     public void setDims(Integer[] dims) {
         this.dims = dims;
     }
 
+    //override toString for print out
     @Override
     public String toString() {
         return "TinyGif{" +
